5f24bbd9b7058a3360a8d5eb7518bec3c3fd3802
zipikaka <2270299018@qq.com>
2024-04-19 21:05:31 +0800

    11

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,17 +1,49 @@
 // index.js
 Page({
-
-data:{
-    a:"通知公告",
-    is1:"block",
-    is2:"none",
-    is3:"none",
-    },
-
-notice1(){
+  data:{
+  a:"通知公告",
+  pb1:"block",
+  pb2:"none",
+  pb3:"none",
+  // block：设置为display的默认值
+      // none：隐藏对象
+  },
+  
+  notice1(){
+    this.setData({
+      a:"通知公告",
+      pb1:"block",
+      pb2:"none",
+      pb3:"none"
+  
+    })
+  
+  },
+  notice2(){
+    this.setData({
+      a:"综合信息",
+      pb1:"none",
+      pb2:"block",
+      pb3:"none",
+      
+  
+    })
+  },
+  notice3(){
+    // this.setDate :修改页面数据的函数
+    this.setData({
+      a:"学术动态",
+      pb1:"none",
+      pb2:"none",
+      pb3:"block"
+    })
+  },
+  onLoad(options){
+    
+  }
   
   
-
-}
-
-})
+  
+  
+  })
+  
--- pages/inter/inter.js
+++ pages/inter/inter.js
@@ -1,11 +1,42 @@
 // pages/inter/inter.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+  a:"通知公告",
+  pb1:"block",
+  pb2:"none",
+  pb3:"none",
+  // block：设置为display的默认值
+      // none：隐藏对象
+  },
+  
+  notice1(){
+    this.setData({
+      a:"通知公告",
+      pb1:"block",
+      pb2:"none",
+      pb3:"none"
+  
+    })
+  
+  },
+  notice2(){
+    this.setData({
+      a:"综合信息",
+      pb1:"none",
+      pb2:"block",
+      pb3:"none",
+      
+  
+    })
+  },
+  notice3(){
+    // this.setDate :修改页面数据的函数
+    this.setData({
+      a:"学术动态",
+      pb1:"none",
+      pb2:"none",
+      pb3:"block"
+    })
   },
 
   /**
